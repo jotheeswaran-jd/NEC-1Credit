e55f6eba3fa16ececc77f3e1fad7aa7dc527809c
jotheeswaran-jd <200459233+jotheeswaran-jd@users.noreply.github.com>
2025-03-09 16:08:09 +0530

    Update personal-api.py

--- personal-api.py
+++ personal-api.py
@@ -1,4 +1,19 @@
-Update this file to add your code
-/name - Should give your name
-/register_number - Should give your register_number
-/department - Should give your department
+from flask import Flask
+
+app =Flask(__name__)
+
+@app.route("/")
+def name():
+    return "Jotheeswaran M"
+
+
+@app.route("/registerNumber")
+def register_number():
+    return "22ITL02"
+
+@app.route("/department")
+def department():
+    return "IT(Information Technology)"
+
+if __name__ == "__main__":
+    app.run(debug=True)
